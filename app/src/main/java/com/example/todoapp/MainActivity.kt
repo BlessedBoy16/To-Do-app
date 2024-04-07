d5c11a51fc54200f2020512422a619d4d9beef4d
BlessedBoy16 <beamerboy2034@gmail.com>
2024-04-07 09:46:42 +0300

    added card

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -1,22 +1,36 @@
 package com.example.todoapp
 
 import android.content.ClipData.Item
+import android.icu.text.CaseMap.Title
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.colorspace.ColorModel.Companion.Rgb
+import androidx.compose.ui.graphics.colorspace.Rgb
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.todoapp.ui.theme.PurpleGrey40
 import com.example.todoapp.ui.theme.TodoAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,16 +52,22 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting() {
-    LazyColumn(
-        modifier=Modifier.fillMaxSize(),
-        contentPadding = PaddingValues(5.dp)
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(3),
+        horizontalArrangement = Arrangement.spacedBy(5.dp),
+        verticalArrangement = Arrangement.spacedBy(5.dp)
+
     ){
     items(count = 100){
-
-        Text(
-            text = "Task$it",
-            fontSize = 15.sp,
-            modifier = Modifier.padding(5.dp))
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .size(0.dp, 100.dp),
+            colors = CardDefaults.cardColors(),
+            border = BorderStroke(1.dp, color = MaterialTheme.colorScheme.surface)
+        ) {
+Text(text = "Title")
         }
+    }
     }
 }
