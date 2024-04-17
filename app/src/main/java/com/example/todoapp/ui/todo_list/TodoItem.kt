247b7e6d5f67ac73e5ce0e9d642bd875b13094a3
BlessedBoy16 <beamerboy2034@gmail.com>
2024-04-17 19:52:44 +0300

    dev/ UI TodoItem

---
+++ app/src/main/java/com/example/todoapp/ui/todo_list/TodoItem.kt
@@ -0,0 +1,63 @@
+package com.example.todoapp.ui.todo_list
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.room.Delete
+import com.example.todoapp.data.Todo
+
+@Composable
+fun TodoItem(
+    todo: Todo,
+    onEvent: (TodoListEvent ) -> Unit,
+    modifier: Modifier = Modifier
+){
+    Row (
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        Column(
+            modifier = modifier.weight( 1f ),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Row (
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Text(
+                    text = todo.title,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                IconButton(onClick = {
+                    onEvent(TodoListEvent.OnDeleteTodoClick(todo))
+                }) {
+                    Icon(
+                        imageVector = Icons.Default.Delete ,
+                        contentDescription = "Delete")
+                    
+                }
+            }
+        }
+        Checkbox(
+            checked = todo.isDone,
+            onCheckedChange = {isCheked ->
+                    onEvent(TodoListEvent.OnDoneChange(todo, isCheked))
+            } )
+    }
+}
